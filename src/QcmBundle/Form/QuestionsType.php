fa78c9eb71acfe571ac912656e3348190a64b41d
Punkoleo <punkoleo@Doliprane>
2016-03-16 19:25:52 +0100

    Selection de la bonne reponse par choix

--- src/QcmBundle/Form/QuestionsType.php
+++ src/QcmBundle/Form/QuestionsType.php
@@ -5,6 +5,7 @@ namespace QcmBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class QuestionsType extends AbstractType
 {
@@ -19,10 +20,16 @@ class QuestionsType extends AbstractType
             ->add('reponseA')
             ->add('reponseB')
             ->add('reponseC')
-            ->add('bonneReponse')
+            ->add('bonneReponse', ChoiceType::class, array(
+                'choices' => array('A'=>'A','B'=>'B','C'=>'C'),
+                'choices_as_values' => true,
+                'multiple' => false,
+                'expanded'=> false,
+                'required' => true,
+            ))
         ;
     }
-    
+
     /**
      * @param OptionsResolver $resolver
      */
